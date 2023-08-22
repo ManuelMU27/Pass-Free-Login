568f71d726f8dd8c0295b113f7c9767774a1c4fc
ManuelMU27 <142941340+ManuelMU27@users.noreply.github.com>
2023-08-22 17:03:29 -0400

    Add files via upload

---
+++ src/views/Login.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from 'vue'
+import vm from '../main'
+
+const email = ref("")
+const isSubmitting = ref(false)
+
+const LoginOrCreate = async () => {
+    if (!email.value) return alert("Please enter an email address")
+    isSubmitting.value = true
+
+    const response = await fetch("http://localhost:5000/loginorcreate", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email.value
+        })
+    }).then(res => res.json())
+
+    if (response.success) {
+        vm.$toast.success(response.message)
+    } else {
+        vm.$toast.error(response.message)
+    }
+
+    isSubmitting.value = false
+}
+</script>
+
+<template>
+    <main class="login min-h-screen bg-gradient-to-br from-green-500 to-cyan-400">
+        <section class="container mx-auto pt-16 pb-4 px-4">
+            <h1 class="text-4xl mb-8 text-center font-black uppercase">
+                Pass-Free Login
+            </h1>
+
+            <form 
+                @submit.prevent="LoginOrCreate"
+                class="bg-gray-700 max-w-md mx-auto rounded p-4">
+                
+                <label class="block mb-2">
+                    <span class="block text-gray-400 mb-2">
+                        Enter your email
+                    </span>
+                    <input type="email" placeholder="e.g. alexsmith@example.com" v-model="email" :disabled="isSubmitting" class="block w-full rounded bg-gray-800 py-2 px-4 disabled:opacity-50" />
+                </label>
+
+                <div class="flex justify-end items-center">
+                    <input type="submit" value="Login" :disabled="isSubmitting" class="incline-block bg-emerald-400 text-lg text-gray-800 font-bold uppercase rounded px-4 py-2 cursor-pointer disabled:opacity-50" />
+                </div>
+                
+            </form>
+        </section>
+    </main>
+</template>
